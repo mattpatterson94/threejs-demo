ad48d9682406f185d8e3d2de706d1b9771519b23
Matt Patterson <matt.patterson@hey.com>
2021-09-24 20:39:02 +1000

    Add lighting

--- src/js/app.ts
+++ src/js/app.ts
@@ -5,7 +5,7 @@ class Card {
 
   constructor () {
     const geometry = new THREE.BoxGeometry(2, 2, 2)
-    const material = new THREE.MeshBasicMaterial({ color: 0xB4D9EF })
+    const material = new THREE.MeshPhongMaterial({ color: 0xB4D9EF })
     this.cube = new THREE.Mesh(geometry, material)
   }
 
@@ -22,17 +22,22 @@ export class App {
   readonly renderer: THREE.WebGLRenderer
   readonly camera: THREE.PerspectiveCamera
   readonly scene: THREE.Scene
+  readonly light: THREE.DirectionalLight
   card?: Card
 
   constructor (element: HTMLElement) {
     this.element = element
     this.renderer = new THREE.WebGLRenderer()
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    this.light = new THREE.DirectionalLight(0xffffff)
     this.scene = new THREE.Scene()
   }
 
   createCard (): void {
     this.card = new Card()
+
+    this.light.position.set(0, 1, 1).normalize()
+    this.scene.add(this.light)
     this.scene.add(this.card.cube)
 
     this.camera.position.z = 5
